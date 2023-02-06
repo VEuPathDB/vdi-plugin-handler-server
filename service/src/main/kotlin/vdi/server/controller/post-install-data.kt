919c22c87369668171b2585c6981f9de322f51cb
Elizabeth <foxcapade@gmail.com>
2023-02-06 16:58:36 -0500

    updates

--- service/src/main/kotlin/vdi/server/controller/post-install-data.kt
+++ service/src/main/kotlin/vdi/server/controller/post-install-data.kt
@@ -2,4 +2,13 @@ package vdi.server.controller
 
 import io.ktor.server.application.*
 
-suspend fun ApplicationCall.handlePostInstallData() {}
+suspend fun ApplicationCall.handlePostInstallData() {
+  // Parse and validate multipart post body
+  // validate details json
+  // create a temp directory
+  // unpack the dataset archive
+  // lookup project id in environment database config
+  // lookup connection string in LDAP
+  // call installer script
+  // handle exit code
+}
