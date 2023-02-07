b2b272c797db9fc2daf612ce0562889a067578b6
Elizabeth <foxcapade@gmail.com>
2023-02-07 11:46:50 -0500

    add log lines

--- service/src/main/kotlin/vdi/service/InstallDataService.kt
+++ service/src/main/kotlin/vdi/service/InstallDataService.kt
@@ -30,15 +30,25 @@ class InstallDataService(
 ) {
   private val log = LoggerFactory.getLogger(javaClass)
 
+  init {
+    log.trace("init(workspace={}, vdiID={}, payload={}, ldap={}, executor={}, dbEnvConfig={}", workspace, vdiID, payload, ldap, executor, dbEnvConfig)
+  }
+
   suspend fun processInstall() : List<String> {
+    log.trace("processInstall()")
+
     val dbLdapConfig = ldap.requireSingularOracleNetDesc(dbEnvConfig.ldap.value)
     val installDir   = workspace.resolve(INSTALL_DIR_NAME)
     val warnings     = ArrayList<String>(4)
 
+    log.debug("creating install data directory {}", installDir)
     installDir.createDirectory()
+
+    log.debug("unpacking {} as a .tar.gz file", payload)
     payload.unpackAsTarGZ(installDir)
     payload.deleteIfExists()
 
+    log.info("executing install-data script for VDI dataset ID {}", vdiID)
     executor.executeScript(
       Configuration.ServiceConfiguration.installDataScriptPath,
       workspace,
@@ -61,8 +71,14 @@ class InstallDataService(
         job2.join()
 
         when (exitCode()) {
-          0    -> {}
-          else -> throw IllegalStateException("install script failed with unexpected exit code")
+          0    -> {
+            log.debug("install-data script completed successfully for VDI dataset ID {}", vdiID)
+          }
+
+          else -> {
+            log.error("install-data script failed for VDI dataset ID {}", vdiID)
+            throw IllegalStateException("install script failed with unexpected exit code")
+          }
         }
       }
     }
