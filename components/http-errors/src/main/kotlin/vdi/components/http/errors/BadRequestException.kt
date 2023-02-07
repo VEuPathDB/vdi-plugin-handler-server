437df229d86e5a40d6c7cdf0e35d502ab68bd95f
Elizabeth <foxcapade@gmail.com>
2023-02-07 13:54:45 -0500

    update exception

--- components/http-errors/src/main/kotlin/vdi/components/http/errors/BadRequestException.kt
+++ components/http-errors/src/main/kotlin/vdi/components/http/errors/BadRequestException.kt
@@ -4,4 +4,5 @@ package vdi.components.http.errors
 class BadRequestException : RuntimeException {
   constructor(message: String) : super(message)
   constructor(cause: Throwable) : super(cause)
+  constructor(message: String, cause: Throwable) : super(message, cause)
 }
