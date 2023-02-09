c2d850b2dd8cb2cf40a9b1218ae2cde7420a3ba6
Elizabeth <foxcapade@gmail.com>
2023-02-09 09:57:02 -0500

    update test

--- components/io-utils/src/test/kotlin/vdi/components/io/LoggingOutputStreamTest.kt
+++ components/io-utils/src/test/kotlin/vdi/components/io/LoggingOutputStreamTest.kt
@@ -9,6 +9,7 @@ import org.slf4j.Logger
 @DisplayName("LoggingOutputStream")
 class LoggingOutputStreamTest {
 
+  val prefix     = "PREFIX"
   val mockLogger = mock(Logger::class.java)
 
   @Test
@@ -16,9 +17,9 @@ class LoggingOutputStreamTest {
   fun t1() {
     val inputStream = "Hello world!".byteInputStream()
 
-    LoggingOutputStream(mockLogger).use { inputStream.transferTo(it) }
+    LoggingOutputStream(prefix, mockLogger).use { inputStream.transferTo(it) }
 
-    verify(mockLogger, times(1)).info("Hello world!")
+    verify(mockLogger, times(1)).info("{} {}", prefix, "Hello world!")
     verifyNoMoreInteractions(mockLogger)
   }
 
@@ -27,11 +28,11 @@ class LoggingOutputStreamTest {
   fun t2() {
     val inputStream = "Goodbye\ncruel\nworld!".byteInputStream()
 
-    LoggingOutputStream(mockLogger).use { inputStream.transferTo(it) }
+    LoggingOutputStream(prefix, mockLogger).use { inputStream.transferTo(it) }
 
-    verify(mockLogger, times(1)).info("Goodbye")
-    verify(mockLogger, times(1)).info("cruel")
-    verify(mockLogger, times(1)).info("world!")
+    verify(mockLogger, times(1)).info("{} {}", prefix, "Goodbye")
+    verify(mockLogger, times(1)).info("{} {}", prefix, "cruel")
+    verify(mockLogger, times(1)).info("{} {}", prefix, "world!")
     verifyNoMoreInteractions(mockLogger)
   }
 
@@ -40,9 +41,9 @@ class LoggingOutputStreamTest {
   fun t3() {
     val inputStream = "Waka waka waka!\n".byteInputStream()
 
-    LoggingOutputStream(mockLogger).use { inputStream.transferTo(it) }
+    LoggingOutputStream(prefix, mockLogger).use { inputStream.transferTo(it) }
 
-    verify(mockLogger, times(1)).info("Waka waka waka!")
+    verify(mockLogger, times(1)).info("{} {}", prefix, "Waka waka waka!")
     verifyNoMoreInteractions(mockLogger)
   }
 }
