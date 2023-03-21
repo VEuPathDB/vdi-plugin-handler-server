8363ffdd02abf4cfddf5737a8ddfb5e0ee4d4a48
Elizabeth <foxcapade@gmail.com>
2023-03-21 19:43:15 -0400

    update the defaults to use the bin directory.

--- service/src/main/kotlin/vdi/consts/ConfigDefault.kt
+++ service/src/main/kotlin/vdi/consts/ConfigDefault.kt
@@ -5,10 +5,10 @@ object ConfigDefault {
   const val ServerPort = "80"
   const val ServerHost = "0.0.0.0"
 
-  const val ImportScriptPath = "/opt/veupathdb/import"
-  const val DataInstallScriptPath = "/opt/veupathdb/install-data"
-  const val MetaInstallScriptPath = "/opt/veupathdb/install-meta"
-  const val UninstallScriptPath = "/opt/veupathdb/uninstall"
+  const val ImportScriptPath = "/opt/veupathdb/bin/import"
+  const val DataInstallScriptPath = "/opt/veupathdb/bin/install-data"
+  const val MetaInstallScriptPath = "/opt/veupathdb/bin/install-meta"
+  const val UninstallScriptPath = "/opt/veupathdb/bin/uninstall"
 
   const val ImportScriptMaxDuration = "1h"
   const val DataInstallScriptMaxDuration = "1h"
