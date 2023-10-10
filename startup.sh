925fad58e98f72389e519f1f0e75c4a107503272
Elizabeth Paige Harper <foxcapades.io@gmail.com>
2023-10-10 10:25:42 -0400

    disable rserve

--- startup.sh
+++ startup.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env bash
 
-R -e "Rserve::Rserve(debug = "${DEBUG}", args=\"--vanilla\")" &
-java -jar -XX:+CrashOnOutOfMemoryError $JVM_MEM_ARGS $JVM_ARGS service.jar &
-wait
+#R -e "Rserve::Rserve(debug = "${DEBUG}", args=\"--vanilla\")" &
+java -jar -XX:+CrashOnOutOfMemoryError $JVM_MEM_ARGS $JVM_ARGS service.jar
