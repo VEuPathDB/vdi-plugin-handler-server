6bde8dd2f8c8058e878824976ee451d86ec6bfc7
Elizabeth Paige Harper <foxcapades.io@gmail.com>
2023-12-14 14:14:37 -0500

    Change expected exit code for validation errors to avoid conflicts with compile errors

--- service/src/main/kotlin/vdi/consts/ExitCode.kt
+++ service/src/main/kotlin/vdi/consts/ExitCode.kt
@@ -2,10 +2,10 @@ package vdi.consts
 
 object ExitCode {
   const val ImportScriptSuccess           = 0
-  const val ImportScriptValidationFailure = 1
+  const val ImportScriptValidationFailure = 99
 
   const val InstallScriptSuccess           = 0
-  const val InstallScriptValidationFailure = 1
+  const val InstallScriptValidationFailure = 99
 
   const val CompatScriptSuccess      = 0
   const val CompatScriptIncompatible = 1
