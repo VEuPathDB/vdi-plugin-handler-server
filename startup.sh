eade2aa2b7bdfd7dd39bb2a993a0186037e42036
Elizabeth Paige Harper <foxcapades.io@gmail.com>
2024-03-18 10:15:41 -0400

    bash exec

--- startup.sh
+++ startup.sh
@@ -1,4 +1,3 @@
 #!/usr/bin/env bash
 
-#R -e "Rserve::Rserve(debug = "${DEBUG}", args=\"--vanilla\")" &
-java -jar -XX:+CrashOnOutOfMemoryError $JVM_MEM_ARGS $JVM_ARGS service.jar
+exec java -jar -XX:+CrashOnOutOfMemoryError $JVM_MEM_ARGS $JVM_ARGS service.jar
